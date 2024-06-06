ccdebdd7112c5d9b780ae761f5dce003f045850a
Altofi00 <alfitodwiaditya87@gmail.com>
2024-06-06 19:53:59 +0700

    update lagii modul 11

--- Modul_11/praktikum.php
+++ Modul_11/praktikum.php
@@ -1,12 +1,14 @@
-<?php 
+<?php
 //Deklarasi tree 
-class Tree {
-    public $data; 
+class Tree
+{
+    public $data;
     public $left;
     public $right;
 
     // pembentukan node baru
-    public function __construct($d){
+    public function __construct($d)
+    {
         $this->data = $d;
         $this->left = null; //mengatur referensi anak kiri ke null
         $this->right = null; //mengatur referensi anak kanan ke null
@@ -14,38 +16,42 @@ class Tree {
 }
 
 //membuat class binarytree
-class BinaryTree{
+class BinaryTree
+{
     public $root; //membuat global variabel agar bisa di akses
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->root = null;
     }
 
     // membuat fungsi insert untuk menambahkan data pada tree
-    public function insert($d){
-        $newNode = new Tree ($d); //membuat node baru 
-        if ($this->root === null){ //jika root bernilai null maka jadikan newNode sebagai root
+    public function insert($d)
+    {
+        $newNode = new Tree($d); //membuat node baru 
+        if ($this->root === null) { //jika root bernilai null maka jadikan newNode sebagai root
             $this->root = $newNode;
-        }else {
+        } else {
             $this->insertNode($this->root, $newNode); //jika tidak maka masukan newNode ke dalam tree
         }
     }
 
     // membuat fungsi rekursif untuk memasukan node baru ke dalam tree
-    private function insertNode(&$node, &$newNode){
-        if ($node == null){ //jika node bernilai null maka jadikan node menjadi newNode
+    private function insertNode(&$node, &$newNode)
+    {
+        if ($node == null) { //jika node bernilai null maka jadikan node menjadi newNode
             $node = $newNode;
-        }else {
-            if ($newNode->data < $node->data){ //jika newNode-data lebih kecil dari node-data maka lakukan seleksi berikut
-                if ($node->left == null){//jika child kiri node bernilai null 
+        } else {
+            if ($newNode->data < $node->data) { //jika newNode-data lebih kecil dari node-data maka lakukan seleksi berikut
+                if ($node->left == null) {//jika child kiri node bernilai null 
                     $node->left = $newNode; // maka pasang newNode sebagai child-kiri
-                }else{
+                } else {
                     $this->insertNode($node->right, $newNode);
                 }
-            }else{ //jika newNode-data lebih besar atau sama dengan node-data maka lakukan seleksi dibawah ini
-                if($node->right == null){ //jika child-kanan bernilai null maka
+            } else { //jika newNode-data lebih besar atau sama dengan node-data maka lakukan seleksi dibawah ini
+                if ($node->right == null) { //jika child-kanan bernilai null maka
                     $node->right = $newNode; //pasang newNode sebagai child-kanan
-                }else{ //selain itu
+                } else { //selain itu
                     $this->insertNode($node->right, $newNode); //lanjutkan ke child-kanan
                 }
             }
@@ -53,8 +59,9 @@ class BinaryTree{
     }
 
     //membuat fungsi untuk melakukan traversal preorder
-    public function preorder($node){
-        if ($node !== null){ //jika node tidak null maka
+    public function preorder($node)
+    {
+        if ($node !== null) { //jika node tidak null maka
             echo $node->data . " "; //tampilkan node-data  yang dikunjungi
             $this->preorder($node->left); //kunjungi left
             $this->preorder($node->right); //kunjungi right
@@ -62,16 +69,18 @@ class BinaryTree{
     }
 
     //membuat fungsi untuk melakukan traversal inorder
-    public function inorder ($node){
-        if ($node !== null){ //jika node tidak null maka
+    public function inorder($node)
+    {
+        if ($node !== null) { //jika node tidak null maka
             $this->inorder($node->left); //kunjungi left
             echo $node->data . " "; //cetak node yang dikunjungi
             $this->inorder($node->right); //kunjungi right
         }
     }
 
-    public function postorder($node){
-        if ($node !== null){ //jika node tidak null maka
+    public function postorder($node)
+    {
+        if ($node !== null) { //jika node tidak null maka
             $this->postorder($node->left); //kunjungi left
             $this->postorder($node->right); //kunjungi right
             echo $node->data . " "; //cetak node yang dikunjungi
@@ -79,17 +88,19 @@ class BinaryTree{
     }
 
     //membuat fungsi cari
-    public function cari ($data){
+    public function cari($data)
+    {
         return $this->cariNode($this->root, $data);
     }
 
     //membuat fungsi untuk menghitung kedalaman tree
-    public function tinggi ($node){
-        if($node == null) //jika node bernilai null maka akan bernilai salah
+    public function tinggi($node)
+    {
+        if ($node == null) //jika node bernilai null maka akan bernilai salah
             return 0;
-        else{ //selain itu
-            $tinggi1 = $this->tinggi ($node->left); //hitung tinggi child-kiri secara rekursif
-            $tinggi2 = $this->tinggi ($node->right); //hitung tinggi child-kanan secara rekursif
+        else { //selain itu
+            $tinggi1 = $this->tinggi($node->left); //hitung tinggi child-kiri secara rekursif
+            $tinggi2 = $this->tinggi($node->right); //hitung tinggi child-kanan secara rekursif
             if ($tinggi1 > $tinggi2) //jika tinggi child kiri lebih besar maka
                 return $tinggi1 + 1; //hitung kedalaman child-kiri ditambah 1
             else //jika kedalaman child-kanan lebih besar atau sama dengan kedalaman child-kiri maka
@@ -98,44 +109,48 @@ class BinaryTree{
     }
 
     //membuat fungsi daun untuk menampilkan daun-daun(node tanpa anak) dalam tree
-    public function daun ($node){
+    public function daun($node)
+    {
         if ($node == null) //jika node bernilai null maka tampilkan tree kosong
             echo "<br>Tree kosong";
         if ($node->left != null) //jika child-kiri tidak bernilai null maka
-            $this->daun ($node->left); //kunjungi child-kiri secara rekursif
+            $this->daun($node->left); //kunjungi child-kiri secara rekursif
         if ($node->right != null) //jika child-kanan tidak bernilai null maka
-            $this->daun ($node->right); //kunjungi child-kanan secara rekursif
+            $this->daun($node->right); //kunjungi child-kanan secara rekursif
         if ($node->left == null && $node->right == null) //jika node-left dan node-right sama sama bernilai null maka 
             echo $node->data, " "; //tampilkan data node saat ini
     }
 
     //membuat privat fungsi cariNode dengan data tertentu
-    private function cariNode ($node, $data){
-    if ($node == null) //jika node saat ini null, maka data tidak ditemukan
-        return 0; //bernilai false
-    if ($node->data == $data) //jika node-data sama dengan data maka data ditemukan
-        return 1; // bernilai true
-    if ($data < $node->data){ //jika data lebih kecil dari node-data maka
-        return $this->cariNode($node->left, $data); //lanjutkan ke child-kiri
-    }else{ //selain itu jika data lebih besar dari node-data maka
-        return $this->cariNode($node->right, $data); //lanjutkan ke child-kanan
-    }
+    private function cariNode($node, $data)
+    {
+        if ($node == null) //jika node saat ini null, maka data tidak ditemukan
+            return 0; //bernilai false
+        if ($node->data == $data) //jika node-data sama dengan data maka data ditemukan
+            return 1; // bernilai true
+        if ($data < $node->data) { //jika data lebih kecil dari node-data maka
+            return $this->cariNode($node->left, $data); //lanjutkan ke child-kiri
+        } else { //selain itu jika data lebih besar dari node-data maka
+            return $this->cariNode($node->right, $data); //lanjutkan ke child-kanan
+        }
     }
 
     //membuat fungsi cek untuk mengecek apakah data tertentu berada dalam tree
-    public function cek($data){
-        if ($this->cari($data)){ //jika data ditemukan
+    public function cek($data)
+    {
+        if ($this->cari($data)) { //jika data ditemukan
             echo "ditemukan";
-        }else{
+        } else {
             echo "Tidak Ditemukan"; //jika data tidak ditemukan
         }
     }
 
     //membuat fungsi hitung untuk menghitung jumlah node yang ada pada tree
-    public function hitung($node){
-        if($node == null){ //jika node bernilai null maka 
+    public function hitung($node)
+    {
+        if ($node == null) { //jika node bernilai null maka 
             return 0; //kembalikan false
-        }else{ //selain itu jika node tidak null maka
+        } else { //selain itu jika node tidak null maka
             return 1 + $this->hitung($node->left) + $this->hitung($node->right); //hitung node saat ini dan childnya secara rekursif
         }
     }
@@ -151,15 +166,20 @@ $tr->insert(33);
 $tr->insert(8);
 
 //cetak hasil traversal preorder
-echo "Preorder : "; $tr->preorder($tr->root);
+echo "Preorder : ";
+$tr->preorder($tr->root);
 //cetak hasil traversal inorder
-echo "<br>Inorder : "; $tr->inorder($tr->root);
+echo "<br>Inorder : ";
+$tr->inorder($tr->root);
 //cetak hasil traversal postorder
-echo "<br>Postorder : "; $tr->postorder($tr->root);
+echo "<br>Postorder : ";
+$tr->postorder($tr->root);
 
 //menampilkan fungsi cek untuk mengecek apakah data yang dicari ada dalam tree atau tidak
-echo "<br>Data 51 : "; $tr->cek(51);
-echo "<br>Data 33 : ";$tr->cek(33);
+echo "<br>Data 51 : ";
+$tr->cek(51);
+echo "<br>Data 33 : ";
+$tr->cek(33);
 
 //menampilkan jumlah node yang ada dalam tree
 echo "<br>Jumlah Node : ", $tr->hitung($tr->root);
